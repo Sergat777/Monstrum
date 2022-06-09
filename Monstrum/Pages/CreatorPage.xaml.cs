3f135d807e992c0b2430da74adcfec0c1d0e9494
Sergat 777 <sergey.kond777@gmail.com>
2022-06-09 06:41:05 +0300

    Add project files.

---
+++ Monstrum/Pages/CreatorPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monstrum.Pages
+{
+    /// <summary>
+    /// Interaction logic for CreatorPage.xaml
+    /// </summary>
+    public partial class CreatorPage : Page
+    {
+        public CreatorPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btReturn_Click(object sender, RoutedEventArgs e)
+        {
+            Classes.ControllerManager.MainAppFrame.GoBack();
+        }
+    }
+}
---
+++ Monstrum/Pages/MainMenuPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Monstrum.Pages
+{
+    /// <summary>
+    /// Interaction logic for MainMenuPage.xaml
+    /// </summary>
+    public partial class MainMenuPage : Page
+    {
+        public MainMenuPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btPlay_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void btCreator_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Classes.ControllerManager.MainAppFrame.Navigate(new CreatorPage());
+        }
+
+        private void btClose_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
